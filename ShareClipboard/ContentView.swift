7d826b00acb3490970fdf842d7a668383ad59e8c
Paul Schwind <git@pschwind.de>
2024-04-13 19:52:15 +0200

    Initial Commit

---
+++ ShareClipboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShareClipboard
+//
+//  Created by Paul Schwind on 13.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShareClipboard/ShareClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareClipboardApp.swift
+//  ShareClipboard
+//
+//  Created by Paul Schwind on 13.04.24.
+//
+
+import SwiftUI
+
+@main
+struct ShareClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
