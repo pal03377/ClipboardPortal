c28c8544071e450c31491bb27bffdc63d4724f75
Paul Schwind <git@pschwind.de>
2024-04-13 22:41:22 +0200

    Initial app

--- ShareClipboard/ContentView.swift
+++ ShareClipboard/ContentView.swift
@@ -1,21 +1,82 @@
-//
-//  ContentView.swift
-//  ShareClipboard
-//
-//  Created by Paul Schwind on 13.04.24.
-//
-
 import SwiftUI
+import AppKit
+import GameKit
 
-struct ContentView: View {
+struct ContentView: View, ClipboardContentReceiver {
+    @EnvironmentObject var gameCenterManager: GameCenterManager
+    @State var lastSharedClipboardEntry: String = ""
+    @State private var commandVListener: Any?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if !gameCenterManager.isSignedIn {
+            Button(action: {
+                // Show Sign In sheet
+                if let window = NSApplication.shared.windows.first, let rootViewController = window.contentViewController, let gkViewControllerForSignIn = gameCenterManager.gkViewControllerForSignIn {
+                    rootViewController.presentAsModalWindow(gkViewControllerForSignIn)
+                }
+            }) {
+                Text("Sign in")
+            }
+            .buttonStyle(.plain)
+        } else if !gameCenterManager.matchedWithPlayers {
+            VStack {
+                Text("Signed in! 🎉")
+                Button(action: {
+                    do {
+                        try GKLocalPlayer.local.presentFriendRequestCreator(from: NSApplication.shared.mainWindow)
+                    } catch {
+                        print("Error: \(error.localizedDescription).")
+                    }
+                }) {
+                    Image(systemName: "person.crop.circle.badge.plus")
+                }
+                Button(action: {
+                    if let window = NSApplication.shared.mainWindow, let rootViewController = window.contentViewController {
+                            gameCenterManager.presentMatchmaker(from: rootViewController)
+                        }
+                }) {
+                    Text("Start clipboard sharing")
+                }
+                .padding()
+            }
+        } else { // Matched with players?
+            VStack {
+                Text("Press Cmd+V to paste from clipboard")
+                Text("Last shared clipboard entry: \(lastSharedClipboardEntry)")
+            }
+            .padding()
+            .onAppear() {
+                // Receive clipboard content
+                self.gameCenterManager.addReceiver(self)
+                // Setup Cmd+V keyboard shortcut to send clipboard content
+                self.commandVListener = NSEvent.addLocalMonitorForEvents(matching: .keyDown) { (event) -> NSEvent? in
+                    if event.modifierFlags.intersection(.deviceIndependentFlagsMask) == .command && event.characters == "v" {
+                        // Read clipboard
+                        let pasteboard = NSPasteboard.general
+                        if let string = pasteboard.string(forType: .string) {
+                            print("Paste string: \(string)")
+                            self.lastSharedClipboardEntry = string
+                            gameCenterManager.sendDataToAllPlayers(message: string)
+                            return nil
+                        }
+                    }
+                    return event
+                }
+            }
+            .onDisappear() {
+                // Clean up Command V listener
+                if let commandVListener = self.commandVListener { NSEvent.removeMonitor(commandVListener) }
+                self.commandVListener = nil
+            }
         }
-        .padding()
+    }
+    
+    func receiveClipboardContent(content: String) {
+        self.lastSharedClipboardEntry = content // Store received content to show it in the UI
+        // Put new content in clipboard
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents() // Clear clipboard to avoid still having an image in it if a text is pasted
+        pasteboard.setString(content, forType: .string)
     }
 }
 
--- ShareClipboard/ShareClipboardApp.swift
+++ ShareClipboard/ShareClipboardApp.swift
@@ -1,17 +1,126 @@
-//
-//  ShareClipboardApp.swift
-//  ShareClipboard
-//
-//  Created by Paul Schwind on 13.04.24.
-//
-
 import SwiftUI
+import GameKit
+import AppKit
+
+
+protocol ClipboardContentReceiver {
+    func receiveClipboardContent(content: String)
+}
+
+class GameCenterManager: NSObject, ObservableObject, GKMatchmakerViewControllerDelegate, GKMatchDelegate {
+    @Published var isSignedIn: Bool = false
+    @Published var matchedWithPlayers: Bool = false
+    var gkViewControllerForSignIn: NSViewController?
+    private var match: GKMatch?
+    private var receivers: [ClipboardContentReceiver] = []
+
+    override init() {
+        super.init()
+        authenticatePlayer()
+    }
+    
+    func authenticatePlayer() {
+        GKLocalPlayer.local.authenticateHandler = { viewController, error in
+            if let gkViewController = viewController {
+                // Don't present the view controller yet, just update the state
+                DispatchQueue.main.async {
+                    self.isSignedIn = false
+                    self.gkViewControllerForSignIn = gkViewController // Store view controller to show when Sign In button is clicked
+                }
+            } else if GKLocalPlayer.local.isAuthenticated {
+                DispatchQueue.main.async {
+                    self.isSignedIn = true
+                }
+                print("Player is authenticated")
+            } else {
+                DispatchQueue.main.async {
+                    self.isSignedIn = false
+                }
+                print("User authentication failed: \(error?.localizedDescription ?? "Unknown error")")
+            }
+        }
+    }
+    
+    func presentMatchmaker(from rootViewController: NSViewController) {
+        let request = GKMatchRequest()
+        request.minPlayers = 2 // The minimum number of players in the match
+        request.maxPlayers = 2 // The maximum number of players in the match
+        request.inviteMessage = "Let's share clipboards with each other!" // Optional custom invitation message
+        if let matchmakerVC = GKMatchmakerViewController(matchRequest: request) {
+            matchmakerVC.matchmakerDelegate = self
+            rootViewController.presentAsModalWindow(matchmakerVC)
+        }
+    }
+    
+    func matchmakerViewControllerWasCancelled(_ viewController: GKMatchmakerViewController) {
+        // Handle cancellation here
+        print("Matchmaking was cancelled by the user.")
+        viewController.dismiss(nil)
+        self.matchedWithPlayers = false
+    }
+    
+    func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFailWithError error: Error) {
+        // Handle errors here
+        print("Matchmaking failed with error: \(error.localizedDescription)")
+        viewController.dismiss(nil)
+        self.matchedWithPlayers = false
+    }
+    
+    func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFind match: GKMatch) {
+        // A match has been found and you can start the game
+        print("A match was found.")
+        viewController.dismiss(nil)
+        self.matchedWithPlayers = true
+        match.delegate = self // Receive events myself
+        self.match = match
+    }
+    
+    // Register to receive messages
+    func addReceiver(_ receiver: ClipboardContentReceiver) {
+        self.receivers.append(receiver)
+    }
+    
+    // Receive message
+    func match(_ match: GKMatch, didReceive data: Data, fromRemotePlayer player: GKPlayer) {
+        if let message = String(data: data, encoding: .utf8) {
+            // Handle the received message string here
+            print("Received message: \(message) from player: \(player.alias)")
+            for receiver in self.receivers {
+                receiver.receiveClipboardContent(content: message)
+            }
+        } else {
+            print("Failed to decode the received data")
+        }
+    }
+    
+    // Send message
+    func sendDataToAllPlayers(message: String) {
+        guard let messageData = message.data(using: .utf8) else {
+            print("Failed to encode the message into data")
+            return
+        }
+        guard let match = self.match else {
+            print("No match exists yet")
+            return
+        }
+        
+        do {
+            try match.sendData(toAllPlayers: messageData, with: .reliable)
+        } catch {
+            print("Failed to send data: \(error.localizedDescription)")
+        }
+    }
+}
+
 
 @main
-struct ShareClipboardApp: App {
+struct ShareClipboardApp: App {    
+    @StateObject private var gameCenterManager = GameCenterManager()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(gameCenterManager)
         }
     }
 }
