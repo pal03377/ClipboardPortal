a6b048be6673f98f9d9abf485591885519b135c6
Paul Schwind <git@pschwind.de>
2024-04-20 04:50:22 +0200

    Put data in dir for better docker compose compatibility

--- server/Sources/App/entrypoint.swift
+++ server/Sources/App/entrypoint.swift
@@ -18,8 +18,13 @@ enum Entrypoint {
         do {
             // Serve files from /Public folder
             app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+            // Create data dir if it doesn't exist
+            let dataDir = app.directory.workingDirectory + "data"
+            if !FileManager.default.fileExists(atPath: dataDir) {
+                try FileManager.default.createDirectory(atPath: dataDir, withIntermediateDirectories: true, attributes: nil)
+            }
             // Set up database
-            app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+            app.databases.use(.sqlite(.file("data/db.sqlite")), as: .sqlite)
             // Create tables
             app.migrations.add(CreateUser())
             try await app.autoMigrate() // Run migrations
